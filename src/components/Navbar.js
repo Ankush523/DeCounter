c8a5c52e2650c616a72ffa32b4ef4d1d1e6188d9
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-06-27 14:21:04 +0530

    colors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const Navbar = () => {
     return ( 
-        <div className='flex flex-row w-[100vw] h-[fit-content] p-[20px] shadow-xl justify-between '  >
-            <label className='text-blue-500 font-bold text-2xl' >DeCounter</label>
+        <div className='flex flex-row w-[100vw] bg-slate-900 h-[fit-content] p-[20px] shadow-xl justify-between '  >
+            <label className='font-serif text-blue-400 font-light text-3xl' >DeCounter</label>
             <ConnectButton/>
         </div>
      );
